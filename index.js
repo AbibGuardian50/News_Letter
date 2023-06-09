b8830bff13a95a57937c7be9723e3064ca9b27d3
Muhammad Abieb Basnuril <83066044+AbibGuardian50@users.noreply.github.com>
2023-06-10 00:11:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ let success =  document.getElementById('success-subscribing');
 let errorAlert = document.getElementById('error-alert');
 let emailMsg = document.getElementById("email-message");
 let emailInput = document.getElementById("email-field");
+let success_icon= document.getElementById("success-icon-subscribing");
+let dismiss_btn = document.getElementById("dismiss-btn");
 let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function ValidateEmail(inputText) {
-  
+    
     if (inputText.value.match(mailformat)) {
         main.style.display = 'none';
-        success.style.display = 'block';
+        // success.style.display = 'flex';
+        success_icon.style.display = 'flex';
         emailMsg.textContent = emailInput.value;
-        return true;
     }
     else {
         errorAlert.style.display = 'block';
         emailInput.style.color = 'red';
         emailInput.style.border = '1px solid red';
-        return false;
     }
+}
+
+function dismiss() {
+    main.style.display = 'block';
+    success_icon.style.display = 'none';
 }
